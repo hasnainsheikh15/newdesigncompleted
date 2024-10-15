0914f20737c30af56282ce1d088f1aff1499f80e
hasnainsheikh15 <163677951+hasnainsheikh15@users.noreply.github.com>
2024-10-16 03:53:46 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-custom":
+          "linear-gradient(to right, #C600EE 0%, #2D1FF0 100%)",
+      },
+    },
+  },
+  plugins: [],
+};
+
